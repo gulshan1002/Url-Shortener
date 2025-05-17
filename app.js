147407e190ddbb33747027802c59c375dd600b2a
Gulshan Kumar <gulshan190210@gmail.com>
2025-05-17 09:12:01 +0530

    add Rate Limit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 const connectDB = require('./config/db');
 const urlRoutes = require('./routes/urlRoutes')
@@ -6,6 +7,12 @@ const urlRoutes = require('./routes/urlRoutes')
 const app = express();
 
 connectDB();
+
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100,
+    message: 'Too many requests, please try again after 15 minutes'
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
